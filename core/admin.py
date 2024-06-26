6b0ccc72e97358bcd78429a29695378677d19e9c
malkoG <rijgndqw012@gmail.com>
2024-06-27 06:32:42 +0900

    Add ModelAdmin for HostedFile

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from core.models import HostedFile
 
 # Register your models here.
+
+@admin.register(HostedFile)
+class HostedFileAdmin(admin.ModelAdmin):
+    list_display = ["node_id", "name", "preview", "created_at"]
+
+    @admin.display(description="미리보기")
+    def preview(self, obj):
+        return format_html(f"<img src='{obj.attachment.url}'/ alt='{obj.name}'>")
