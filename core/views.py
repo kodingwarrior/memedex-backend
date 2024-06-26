a28b523ac349caebe78205ab52b391947ebb50f5
malkoG <rijgndqw012@gmail.com>
2024-06-27 06:34:35 +0900

    Add API for file uploads using NinjaAPI and integrate with HostedFile model

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 from django.views.generic import TemplateView
+from ninja import NinjaAPI, Swagger, UploadedFile, Form, Schema, File
+
+from core.models import HostedFile, VNode
 
 
 class SampleView(TemplateView):
@@ -10,3 +11,13 @@ class SampleView(TemplateView):
 
 class SigninView(TemplateView):
     template_name = "example/signin.jinja"
+
+
+api = NinjaAPI(docs=Swagger(settings={"persistAuthorization": True}))
+
+@api.post('/attachments')
+def upload_attachment(request, file: File[UploadedFile]):
+    hosted_file = HostedFile(name=file.name, attachment=file)
+    hosted_file.save()
+    data = file.read()
+    return {'name': hosted_file.name, 'len': len(data)}
