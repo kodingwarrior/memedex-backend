b18315b8e3eeb7ec675ee0212a1dfa1fc0daf057
malkoG <rijgndqw012@gmail.com>
2024-06-27 06:42:51 +0900

    Install django-ninja to support fast development of REST API

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core.views import SampleView, SigninView
+from core.views import SampleView, SigninView, api
 
 urlpatterns = [
+    path('api/', api.urls),
     path("sample", SampleView.as_view()),
     path("signin", SigninView.as_view()),
 ]
