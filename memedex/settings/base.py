99e1059d104852e4752aebdc5a9be9867060e529
malkoG <rijgndqw012@gmail.com>
2024-07-15 01:38:38 +0900

    Install whitenoise in order to allow mimetypes(css/js)

--- memedex/settings/base.py
+++ memedex/settings/base.py
@@ -49,6 +49,8 @@ TAILWIND_APP_NAME = 'memedex_theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
