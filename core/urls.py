56317bd031b3b720c96a7e885907696a385fa4d9
malkoG <rijgndqw012@gmail.com>
2024-07-15 00:35:07 +0900

    Fix static files not loading in production by updating URL patterns

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http.response import HttpResponse
 from django.urls import path
 from core.views import GalleryView, UploadView, SigninView, api
@@ -11,4 +13,4 @@ urlpatterns = [
     path("upload", UploadView.as_view()),
     path("gallery", GalleryView.as_view()),
     path("signin", SigninView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
