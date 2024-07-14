28c5f5137f2f3d02db709ba85585117de47d7b2e
malkoG <rijgndqw012@gmail.com>
2024-07-14 22:11:10 +0900

    Switch database from SQLite to PostgreSQL and add psycopg2 dependency

--- memedex/settings/base.py
+++ memedex/settings/base.py
@@ -106,8 +106,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'memedex'),
+        'USER': os.environ.get('POSTGRES_USER', os.environ.get('USER')),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
