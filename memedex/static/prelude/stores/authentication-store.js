a2442849fd0db51da8514f64db78062746b89d83
malkoG <rijgndqw012@gmail.com>
2024-07-14 16:04:46 +0900

    (frontend) Introduce authenticationStore in order to manage jwt token

---
+++ memedex/static/prelude/stores/authentication-store.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
+
+export const useAuthenticationStore = defineStore('authentication', {
+  state: () => ({
+    jwtToken: useLocalStorage('jwtToken', null),
+  }),
+  getters: {
+    isAuthenticated(state) {
+      return state?.jwtToken !== null
+    }
+  },
+  actions: {
+    async login(username, password) {
+      const response = await fetch('/api/token/pair', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      const { access: token } = await response.json()
+      this.jwtToken = token
+    },
+    logout() {
+      this.jwtToken = null
+    },
+  },
+});
