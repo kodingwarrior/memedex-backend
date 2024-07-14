80689b6445052a385c6fad1732d937c0e320e331
malkoG <rijgndqw012@gmail.com>
2024-07-14 23:44:16 +0900

    Add healthcheck endpoint to urls.py (for kamal deploy)

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
+from django.http.response import HttpResponse
 from django.urls import path
 from core.views import GalleryView, UploadView, SigninView, api
 
+def healthcheck(request):
+    return HttpResponse(status=200)
+
 urlpatterns = [
+    path('up', healthcheck),
     path('api/', api.urls),
     path("upload", UploadView.as_view()),
     path("gallery", GalleryView.as_view()),
