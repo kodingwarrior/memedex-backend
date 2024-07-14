eb33ca1d99f47ef6def11a97a6043e7753fa811c
malkoG <rijgndqw012@gmail.com>
2024-07-15 00:01:52 +0900

    Enable verbose logging temporarily in production for debugging purposes

--- memedex/settings/production.py
+++ memedex/settings/production.py
@@ -18,3 +18,25 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "memedex.kodingwarrior.dev",
 ]
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "DEBUG"),
+            "propagate": False,
+        },
+    },
+}
