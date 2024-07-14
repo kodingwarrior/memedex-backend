580e91b3874bc09cb0e33da487507e591fc31e21
malkoG <rijgndqw012@gmail.com>
2024-07-14 16:25:27 +0900

    (frontend) Implement auth handler with Pinia in DefaultLayout

--- memedex/static/prelude/layouts/default-layout.js
+++ memedex/static/prelude/layouts/default-layout.js
@@ -1,6 +1,23 @@
+import { mapActions, mapState, mapStores } from "pinia";
+
 import NavigationMenu from "prelude/components/navigation-menu";
+import { useAuthenticationStore } from "prelude/stores/authentication-store";
 
 const DefaultLayout = {
+  props: ['login-required'],
+  computed: {
+    ...mapStores(useAuthenticationStore),
+    ...mapState(useAuthenticationStore, ['isAuthenticated']),
+  },
+  methods:{
+    ...mapActions(useAuthenticationStore, ['login', 'logout']),
+  },
+  setup(props) {
+    const store = useAuthenticationStore();
+    if (props.loginRequired !== undefined && !store.isAuthenticated) {
+      window.location.href = '/signin';
+    }
+  },
   template: `
     <div class="w-full mx-4 xl:mx-auto xl:max-w-3xl">
       <navigation-menu></navigation-menu>
