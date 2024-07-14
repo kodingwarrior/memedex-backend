b7ec562bab67f05c52ab9ca0a232385a30188f10
malkoG <rijgndqw012@gmail.com>
2024-07-14 16:49:39 +0900

    Add login functionality to SigninPage with form submission and authentication handling

--- core/static/memedex/pages/signin-page.js
+++ core/static/memedex/pages/signin-page.js
@@ -1,17 +1,55 @@
+import { mapActions, mapState, mapStores } from "pinia";
+import { useAuthenticationStore } from "prelude/stores/authentication-store";
+
+
 import DefaultLayout from 'prelude/layouts/default-layout';
 
-import AspectRatio from 'prelude/components/aspect-ratio';
-import NavigationMenu from 'prelude/components/navigation-menu'
-
 const SigninPage = {
+  computed: {
+    ...mapStores(useAuthenticationStore),
+    ...mapState(useAuthenticationStore, ['isAuthenticated']),
+  },
+  methods:{
+    ...mapActions(useAuthenticationStore, ['login', 'logout']),
+    onSubmit(e) {
+      e.preventDefault();
+      this.login(this.username, this.password);
+    }
+  },
+  setup(props) {
+    const store = useAuthenticationStore();
+    if (store.isAuthenticated) {
+      window.location.href = '/gallery';
+    }
+  },
 	template: `
-	<default-layout>
-	  <aspect-ratio/>
-	</default-layout>
+    <default-layout>
+      <div class="w-full max-w-md mx-auto px-12">
+        <form @submit="onSubmit">
+          <div>
+            <label>
+              username
+            </label>
+            <input v-model="username" />
+          </div>
+
+          <div>
+            <label>
+              password
+            </label>
+            <input type="password" v-model="password" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </default-layout>
 	`,
+  data: () => ({
+    username: "",
+    password: "",
+  }),
 	components: { 
-		NavigationMenu,
-		AspectRatio,
     DefaultLayout
 	},
 }
