f4d44c6f19f7705bd9d2edda88f64842ede63edd
malkoG <rijgndqw012@gmail.com>
2024-07-14 23:20:22 +0900

    Add static files configuration to production settings

---
+++ memedex/settings/production.py
@@ -0,0 +1,14 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+from .base import *
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES['staticfiles'] = {
+    "BACKEND": "django.core.files.storage.FileSystemStorage",
+    "OPTIONS": {
+        "location": STATIC_ROOT,
+        "base_url": "/static",
+    }
+}
